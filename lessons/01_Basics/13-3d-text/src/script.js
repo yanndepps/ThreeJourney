157e1fa585913cd7eafb3360172c226c525588ae
yanndepps <yann.deperrot@protonmail.com>
2022-08-03 14:48:33 +0200

    done 3d-text

--- lessons/01_Basics/13-3d-text/src/script.js
+++ lessons/01_Basics/13-3d-text/src/script.js
@@ -1,4 +1,3 @@
-// 13.24
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
@@ -18,10 +17,13 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Textures
- */
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
+// Textures
 const textureLoader = new THREE.TextureLoader()
+const matcapTex = textureLoader.load('/textures/matcaps/7.png')
 
 
 // Fonts
@@ -30,22 +32,54 @@ fontLoader.load(
   '/fonts/helvetiker_regular.typeface.json',
   (font) => {
     const textGeometry = new TextGeometry(
-      'a saucer full of secrets',
+      'be the change',
       {
         font: font,
         size: 0.5,
         height: 0.2,
-        curveSegments: 12,
+        curveSegments: 6,
         bevelEnabled: true,
         bevelThickness: 0.03,
         bevelSize: 0.02,
         bevelOffset: 0,
-        bevelSegments: 5
+        bevelSegments: 4
       }
     )
-    const textMaterial = new THREE.MeshBasicMaterial()
-    const text = new THREE.Mesh(textGeometry, textMaterial)
+    // textGeometry.computeBoundingBox()
+    // textGeometry.translate(
+    //   - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+    //   - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    //   - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+    // )
+    // Same as above ...
+    textGeometry.center()
+
+    const material = new THREE.MeshMatcapMaterial({ matcap: matcapTex })
+    const text = new THREE.Mesh(textGeometry, material)
     scene.add(text)
+
+    // Objects
+    console.time('donuts')
+
+    const donutGeom = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+    for (let i = 0; i < 100; i++) {
+      const donut = new THREE.Mesh(donutGeom, material)
+
+      donut.position.x = (Math.random() - 0.5) * 10
+      donut.position.y = (Math.random() - 0.5) * 10
+      donut.position.z = (Math.random() - 0.5) * 10
+
+      donut.rotation.x = Math.random() * Math.PI
+      donut.rotation.y = Math.random() * Math.PI
+
+      const scale = Math.random() * 0.75
+      donut.scale.set(scale, scale, scale)
+
+      scene.add(donut)
+    }
+    console.timeEnd('donuts')
+
   }
 )
 
