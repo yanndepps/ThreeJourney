bfe1ca3109b155dd406528cd3183616dcd076e2b
yanndepps <yann.deperrot@protonmail.com>
2023-01-15 22:07:53 +0100

    wip raycaster

--- lessons/02_Classic_Techniques/20-raycaster/src/script.js
+++ lessons/02_Classic_Techniques/20-raycaster/src/script.js
@@ -19,45 +19,47 @@ const scene = new THREE.Scene()
  * Objects
  */
 const object1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+	new THREE.SphereGeometry(0.5, 16, 16),
+	new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 object1.position.x = - 2
 
 const object2 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+	new THREE.SphereGeometry(0.5, 16, 16),
+	new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 
 const object3 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+	new THREE.SphereGeometry(0.5, 16, 16),
+	new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
 
+// Raycaster
+const raycaster = new THREE.Raycaster()
+
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+	width: window.innerWidth,
+	height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+	// Update sizes
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+	// Update camera
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -76,7 +78,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+	canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -86,18 +88,39 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+	// animate objects
+	object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+	object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+	object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
-    // Render
-    renderer.render(scene, camera)
+	// cast a ray
+	// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+	// const rayDirection = new THREE.Vector3(1, 0, 0)
+	// rayDirection.normalize()
+	// raycaster.set(rayOrigin, rayDirection)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+	// const objectsToTest = [object1, object2, object3]
+	// const intersects = raycaster.intersectObjects(objectsToTest)
+
+	// for (const object of objectsToTest) {
+	// 	object.material.color.set('#FF0000')
+	// }
+
+	// for (const intersect of intersects) {
+	// 	intersect.object.material.color.set('#0000FF')
+	// }
+
+	// Update controls
+	controls.update()
+
+	// Render
+	renderer.render(scene, camera)
+
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick)
 }
 
 tick()
