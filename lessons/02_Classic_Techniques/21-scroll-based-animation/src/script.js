abaaaeea337d8f4ef69d3be093e102c56fe75e02
yanndepps <yann.deperrot@protonmail.com>
2023-01-18 00:22:57 +0100

    done scroll animation

--- lessons/02_Classic_Techniques/21-scroll-based-animation/src/script.js
+++ lessons/02_Classic_Techniques/21-scroll-based-animation/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from 'gsap'
 
 /**
  * Debug
@@ -14,6 +15,7 @@ const parameters = {
 
 gui.addColor(parameters, 'materialColor').onChange(() => {
 	material.color.set(parameters.materialColor)
+	particlesMat.color.set(parameters.materialColor)
 })
 gui.add(parameters, 'intensity').min(0).max(1).step(0.01).onChange(() => {
 	dirLight.intensity = parameters.intensity
@@ -69,6 +71,27 @@ scene.add(mesh1, mesh2, mesh3)
 
 const sectionMeshes = [mesh1, mesh2, mesh3]
 
+// Particles
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+for (let i = 0; i < particlesCount; i++) {
+	positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+	positions[i * 3 + 1] = objDist * 0.5 - Math.random() * objDist * sectionMeshes.length
+	positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+// init the BufferGeometry and set the pos attribute
+const particlesGeo = new THREE.BufferGeometry()
+particlesGeo.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+// particles material -> PointsMaterial
+const particlesMat = new THREE.PointsMaterial({
+	color: parameters.materialColor,
+	sizeAttenuation: true,
+	size: 0.03
+})
+// create the particles
+const particles = new THREE.Points(particlesGeo, particlesMat)
+scene.add(particles)
+
 // Lights
 const dirLight = new THREE.DirectionalLight('#FFFFFF', parameters.intensity)
 dirLight.position.set(1, 1, 0)
@@ -100,9 +123,12 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
+// add the camera to a Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -116,25 +142,60 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Scroll
 let scrollY = window.scrollY
+let currentSection = 0
+
+
 window.addEventListener('scroll', () => {
 	scrollY = window.scrollY
+	const newSection = Math.round(scrollY / sizes.height)
+	if (newSection != currentSection) {
+		currentSection = newSection
+		gsap.to(sectionMeshes[currentSection].rotation,
+			{
+				duration: 1.5,
+				ease: 'power2.inOut',
+				x: '+=6',
+				y: '+=3',
+				z: '+=1.5'
+			})
+	}
+})
+
+// retrieve the cursor position
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (event) => {
+	// normalize the values ( by dividing them by the viewport' sizes )
+	cursor.x = event.clientX / sizes.width - 0.5
+	cursor.y = event.clientY / sizes.height - 0.5
 })
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () => {
 	const elapsedTime = clock.getElapsedTime()
+	const deltaTime = elapsedTime - previousTime
+	previousTime = elapsedTime
 
 	// animate camera
 	camera.position.y = - scrollY / sizes.height * objDist
 
+	const parallaxX = cursor.x * 0.5
+	const parallaxY = - cursor.y * 0.5
+	// distance from actual pos to destination and apply deltaTime
+	cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+	cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+
 	// animate meshes
 	for (const mesh of sectionMeshes) {
-		mesh.rotation.x = elapsedTime * 0.1
-		mesh.rotation.y = elapsedTime * 0.12
+		mesh.rotation.x += deltaTime * 0.1
+		mesh.rotation.y += deltaTime * 0.12
 	}
 
 	// Render
